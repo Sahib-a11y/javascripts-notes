2d88523868d9ca1d69649f4058f61565f40d5901
Gursahib Singh <aulakhg808@gmail.com>
2025-06-12 13:14:49 +0530

    project 6 color changing with start button.

---
+++ p6/color.js
@@ -0,0 +1,33 @@
+// generate color 
+
+const raandomcolor = function(){
+    const hex  = "0123456789ABCDEF"
+    let color ="#";
+
+    for( let i=0; i<6 ; i++){
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+};
+
+console.log(raandomcolor());
+
+let interval;
+
+const startcolorchanging = function () {
+    if(!interval){
+        interval = setInterval(changecolor,1000)
+    }
+    function changecolor(){
+        document.body.style.backgroundColor = raandomcolor();
+    }
+}
+
+const stoptchangingcolor = function(){
+    clearInterval(interval) 
+    interval = null;
+}
+
+document.querySelector('#start').addEventListener('click', startcolorchanging);
+
+document.querySelector('#stop').addEventListener('click',stoptchangingcolor);
