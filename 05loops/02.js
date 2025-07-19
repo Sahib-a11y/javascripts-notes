f2e1ea709ecc68c8e575945a73e16b50a0c2a5ff
Gursahib Singh <aulakhg808@gmail.com>
2025-07-19 21:19:01 +0530

    loops

--- 05loops/02.js
+++ 05loops/02.js
@@ -1,36 +1,52 @@
-// while loop
+// // while loop
 
-let index = 0;
-while (index<=10) {
-    // console.log(`value is ${index}`);
-    index= index+1
-}
+// let index = 0;
+// while (index<=10) {
+//     // console.log(`value is ${index}`);
+//     index= index+1
+// }
 
-let heros = ["ironman","captain", "hulk"]
-let arr= 0;
-let herosnames =["stack", "steve", "banner"]
-let reallname = 0;
+// let heros = ["ironman","captain", "hulk"]
+// let arr= 0;
+// let herosnames =["stack", "steve", "banner"]
+// let reallname = 0;
 
 
-while(arr<=heros.length){
-    // console.log(`named heros is ${heros[arr]}`);
-    arr = arr+1
+// while(arr<=heros.length){
+//     // console.log(`named heros is ${heros[arr]}`);
+//     arr = arr+1
 
-    while(reallname<=herosnames.length){
-        // console.log(`inner loop ${reallname}`);
-        reallname= reallname+1
-    }
-}
+//     while(reallname<=herosnames.length){
+//         // console.log(`inner loop ${reallname}`);
+//         reallname= reallname+1
+//     }
+// }
 
-// ***********************************************************
+// // ***********************************************************
 
-// do-while Loop
+// // do-while Loop
 
-// in this loop first work done after check condition .
+// // in this loop first work done after check condition .
 
-let score = 0;
+// let score = 0;
 
-do {
-    console.log(`score is ${score}`);   //first print this after
-    score++    //increment in score 
-} while (score <=10); //check this condition
+// do {
+//     console.log(`score is ${score}`);   //first print this after
+//     score++    //increment in score 
+// } while (score <=10); //check this condition
+
+
+// var naam ="sahib"
+
+// console.log(naam)
+// var naam ="arjun"
+// console.log(naam);
+
+
+let naam = ["sahib", "arjun" ]
+naam.push="akash"
+
+
+
+
+
